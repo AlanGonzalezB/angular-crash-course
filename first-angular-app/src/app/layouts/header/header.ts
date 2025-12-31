c270583fd36e99998a19e37a11ecac90a9ab51e3
Alan González <alan.gonzalez@ientc.com>
2025-12-31 13:48:56 -0600

    feat: header started

---
+++ first-angular-app/src/app/layouts/header/header.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+/*
+  Our header will be here since we want to comply with IENTC
+  architecture
+*/
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  standalone: true,
+  styleUrl: './header.css',
+})
+export class Header {
+
+}
