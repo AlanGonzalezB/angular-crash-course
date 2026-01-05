e6c258919c4bdfd58e7c9ac81bfaaf64344689ae
Alan González <alan.gonzalez@ientc.com>
2026-01-05 16:20:41 -0600

    feat: map added, signals understood, only thing left is to finish the project itself

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,13 +1,26 @@
 import { Component, signal } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layouts/header/header";
+import { User } from "./pages/user/user";
+import { DUMMY_USERS } from '../../public/dummy-users'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, User, KeyValuePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('selected user with id: ' + id)
+
+  }
+
+
+
+
   protected readonly title = signal('first-angular-app');
 }
